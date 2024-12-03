b5c42646a54f05dda6b82aebe0bff50cb98165f0
prathammanjhi <prathammanjhi2@gmail.com>
2024-12-03 12:08:17 +0530

    finished and deployed

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 let myLeads = []
-let oldLeads = []
+let oldLeads = [] 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+const tabs = [{url : "https://www.youtube.com"}]
+
+tabBtn.addEventListener("click", function(){
+    
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+    })
+    
+
+    
+})
+
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
